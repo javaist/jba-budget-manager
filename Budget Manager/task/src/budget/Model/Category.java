e7bb4df7991d16378c89ed0dcb5dbd93910ad46c
Dmitry <javaist@xsd.ru>
2020-10-02 18:40:45 +0300

    Stage 5 of 5: Analyzer

--- Budget Manager/task/src/budget/Model/Category.java
+++ Budget Manager/task/src/budget/Model/Category.java
@@ -1,8 +1,22 @@
 package budget.Model;
 
-public enum Category {
-    Food,
-    Clothes,
-    Entertainment,
-    Other
+public class Category {
+    private String name;
+    private double sum;
+
+    public Category(String value) {
+        this.name = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSum() {
+        return sum;
+    }
+
+    public void setSum(double sum) {
+        this.sum = sum;
+    }
 }
--- Budget Manager/task/src/budget/Presenter/Presenter.java
+++ Budget Manager/task/src/budget/Presenter/Presenter.java
@@ -10,21 +10,42 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
 
 public class Presenter {
     View view = new View();
     ArrayList<Transaction> transactions = new ArrayList<>();
     Scanner scanner = new Scanner(System.in);
     String fileName = "purchases.txt";
+    final HashMap<String, Category> categoryByName = new HashMap<>();
+    final HashMap<Integer, Category> categoryByOrder = new HashMap<>();
+
+    {
+        Category category;
+        String[] categories = {"Food", "Clothes", "Entertainment", "Other"};
+        for (int i = 0; i < categories.length; i++) {
+            category = new Category(categories[i]);
+            categoryByName.put(categories[i], category);
+            categoryByOrder.put(i + 1, category);
+        }
+    }
 
     public void run() {
         int choice;
         do {
-            choice = this.selectAction();
+            view.requestMessage("Choose your action:");
+            choice = this.selectAction(
+                    new String[]{
+                            "1) Add income",
+                            "2) Add purchase",
+                            "3) Show list of purchases",
+                            "4) Balance",
+                            "5) Save",
+                            "6) Load",
+                            "7) Analyze (Sort)",
+                            "0) Exit"
+                    }
+            );
             view.breakLine();
             switch (choice) {
                 case 1:
@@ -45,9 +66,123 @@ public class Presenter {
                 case 6:
                     this.loadFromFile();
                     break;
+                case 7:
+                    this.analyze();
+                    break;
             }
         } while (choice != 0);
         view.resultMessage("Bye!");
+    }
+
+    private void analyze() {
+        int choice;
+        do {
+            view.requestMessage("How do you want to sort?");
+            choice = selectAction(new String[]{
+                    "1) Sort all purchases",
+                    "2) Sort by type",
+                    "3) Sort certain type",
+                    "4) Back"
+            }, 1);
+            view.breakLine();
+            switch (choice) {
+                case 1:
+                    this.sort(null);
+                    break;
+                case 2:
+                    this.sortByType();
+                    break;
+                case 3:
+                    this.sortCertainType();
+                    break;
+            }
+
+        } while (choice != 4);
+    }
+
+    private void sort(Category category) {
+        ArrayList<Purchase> purchases = new ArrayList<>();
+        for (Transaction transaction : transactions) {
+            if (transaction instanceof Purchase) {
+                Purchase purchase = (Purchase) transaction;
+                if (category == null ||
+                        Objects.equals(category.getName(), purchase.getCategory().getName())) {
+                    purchases.add((Purchase) transaction);
+                }
+            }
+        }
+
+        if (purchases.size() == 0) {
+            view.resultMessage("Purchase list is empty");
+            return;
+        }
+
+        purchases.sort((purchase, t1) -> {
+            if (purchase.getPrice() > t1.getPrice()) {
+                return -1;
+            } else if (purchase.getPrice() < t1.getPrice()) {
+                return 1;
+            }
+            return 0;
+        });
+        showPurchases(purchases, "All");
+
+    }
+
+    private void sortByType() {
+        ArrayList<Purchase> purchases = new ArrayList<>();
+        for (Transaction transaction : transactions) {
+            if (transaction instanceof Purchase) {
+                purchases.add((Purchase) transaction);
+            }
+        }
+
+        ArrayList<Category> categories = new ArrayList<>();
+        HashMap<String, Double> categorySum = new HashMap<>();
+        for (Purchase purchase : purchases) {
+            String key = purchase.getCategory().getName();
+            if (categorySum.containsKey(key)) {
+                categorySum.replace(key, categorySum.get(key) + purchase.getPrice());
+            } else {
+                categorySum.put(key, purchase.getPrice());
+            }
+        }
+        for (int i = 0; i < categoryByName.size(); i++) {
+            Category category = categoryByOrder.get(i + 1);
+            if (categorySum.containsKey(category.getName())) {
+                category.setSum(categorySum.get(category.getName()));
+            } else {
+                category.setSum(0);
+            }
+            categories.add(category);
+        }
+        categories.sort((cat1, cat2) -> {
+            if (cat1.getSum() > cat2.getSum()) {
+                return -1;
+            } else if (cat1.getSum() < cat2.getSum()) {
+                return 1;
+            }
+            return 0;
+        });
+
+        double total = 0;
+        view.requestMessage("Types:");
+        for (Category category : categories) {
+            view.requestMessage(String.format(
+                    "%s - $%.2f",
+                    category.getName(),
+                    category.getSum())
+            );
+            total += category.getSum();
+        }
+        view.resultMessage(String.format("Total sum: $%.2f", total));
+    }
+
+    private void sortCertainType() {
+        String category = selectCategory(false, false);
+        view.breakLine();
+        this.sort(categoryByName.get(category));
+
     }
 
 
@@ -67,7 +202,7 @@ public class Presenter {
                 String text = decode(fileReader.nextLine());
                 double price = Double.parseDouble(decode(fileReader.nextLine()));
                 if (type.contains("Purchase")) {
-                    Category category = Category.valueOf(decode(fileReader.nextLine()));
+                    Category category = categoryByName.get(decode(fileReader.nextLine()));
                     Purchase transaction = new Purchase(category);
                     transaction.setPrice(price);
                     transaction.setText(text);
@@ -92,7 +227,7 @@ public class Presenter {
                 fileWriter.write(encode(transaction.getText()));
                 fileWriter.write(encode(String.valueOf(transaction.getPrice())));
                 if (transaction instanceof Purchase) {
-                    fileWriter.write(encode(((Purchase) transaction).getCategory().toString()));
+                    fileWriter.write(encode(((Purchase) transaction).getCategory().getName()));
                 }
             }
         } catch (IOException e) {
@@ -118,12 +253,12 @@ public class Presenter {
 
     private void addPurchase() {
         do {
-            String action = selectCategory();
+            String action = selectCategory(false, true);
             view.breakLine();
             if ("Back".equals(action)) {
                 return;
             }
-            Purchase purchase = new Purchase(Category.valueOf(action));
+            Purchase purchase = new Purchase(categoryByName.get(action));
             do {
                 view.requestMessage("Enter purchase name:");
                 purchase.setText(scanner.nextLine());
@@ -142,39 +277,43 @@ public class Presenter {
         } while (true);
     }
 
+    private void showPurchases(ArrayList<Purchase> purchases, String category) {
+        view.requestMessage(category + ":");
+        int counter = 0;
+        double sum = 0;
+        for (Purchase purchase : purchases) {
+            if ("All".equals(category) ||
+                    Objects.equals(purchase.getCategory().getName(), category)) {
+                sum += purchase.getPrice();
+                view.requestMessage(purchase.toString());
+                counter++;
+            }
+        }
+        if (counter == 0) {
+            view.resultMessage("Purchase list is empty");
+        } else {
+            view.resultMessage(String.format("Total sum: $%.2f", sum));
+        }
+    }
 
     private void show() {
-        if (transactions.size() == 0) {
+        ArrayList<Purchase> purchases = new ArrayList<>();
+        for (Transaction transaction : transactions) {
+            if (transaction instanceof Purchase) {
+                purchases.add((Purchase) transaction);
+            }
+        }
+        if (purchases.size() == 0) {
             view.resultMessage("Purchase list is empty");
             return;
         }
         while (true) {
-            String action = selectCategory(true);
+            String action = selectCategory(true, true);
             view.breakLine();
             if ("Back".equals(action)) {
                 return;
             }
-            view.requestMessage(action + ":");
-            int counter = 0;
-            double sum = 0;
-            for (Transaction transaction : transactions) {
-                if (transaction instanceof Purchase) {
-                    Purchase purchase = (Purchase) transaction;
-                    if ("All".equals(action) ||
-                            Objects.equals(purchase.getCategory(), Category.valueOf(action))) {
-                        sum += transaction.getPrice();
-                        view.requestMessage(transaction.toString());
-                        counter++;
-                    }
-                }
-            }
-
-            if (counter == 0) {
-                view.resultMessage("Purchase list is empty");
-
-            } else {
-                view.resultMessage(String.format("Total sum: $%.2f", sum));
-            }
+            showPurchases(purchases, action);
         }
     }
 
@@ -189,69 +328,72 @@ public class Presenter {
 
     }
 
+    private int selectAction(String[] choices) {
+        return selectAction(choices, 0);
+    }
 
-    private int selectAction() {
+
+    private int selectAction(String[] choices, int index) {
         int choice;
-        String[] choices = {
-                "Choose your action:",
-                "1) Add income",
-                "2) Add purchase",
-                "3) Show list of purchases",
-                "4) Balance",
-                "5) Save",
-                "6) Load",
-                "0) Exit"
-        };
+
         do {
             view.menu(choices);
             try {
                 choice = Integer.parseInt(scanner.nextLine());
-                if (0 > choice || choice > choices.length - 2) {
+                if (index > choice || choice > choices.length) {
                     throw new NumberFormatException();
                 }
                 return choice;
             } catch (NumberFormatException e) {
-                view.resultMessage(String.format("Please input a number from 0 to %d", choices.length - 2));
+                view.resultMessage(String.format(
+                        "Please input a number from %d to %d", index,
+                        choices.length - 1 + index));
             }
         } while (true);
     }
 
-    private String selectCategory() {
-        return selectCategory(false);
-    }
-
-    private String selectCategory(boolean all) {
+    private String selectCategory(boolean all, boolean back) {
         int choice;
-        int extra = all ? 3 : 2;
-        String[] choices = new String[Category.values().length + extra];
+        int extra = 1;
+        extra += all ? 1 : 0;
+        extra += back ? 1 : 0;
+        String[] choices = new String[categoryByName.size() + extra];
         choices[0] = "Choose the type of purchase";
 
-        for (int i = 0; i < Category.values().length; i++) {
-            choices[i + 1] = String.format("%d) %s", i + 1, Category.values()[i]);
+        for (int i = 1; i < categoryByOrder.keySet().size() + 1; i++) {
+            choices[i] = String.format("%d) %s", i, categoryByOrder.get(i).getName());
         }
-
-        if (all) {
+        int allChoice = 0;
+        int backChoice = 0;
+        if (all && back) {
             choices[choices.length - 2] = String.format("%d) All", choices.length - 2);
+            allChoice = choices.length - 2;
+            choices[choices.length - 1] = String.format("%d) Back", choices.length - 1);
+            backChoice = choices.length - 1;
+        } else if (all) {
+            choices[choices.length - 1] = String.format("%d) All", choices.length - 1);
+            allChoice = choices.length - 1;
+        } else if (back) {
+            choices[choices.length - 1] = String.format("%d) Back", choices.length - 1);
+            backChoice = choices.length - 1;
         }
-
-        choices[choices.length - 1] = String.format("%d) Back", choices.length - 1);
         do {
             view.menu(choices);
             try {
                 choice = Integer.parseInt(scanner.nextLine());
                 if (1 > choice || choice >= choices.length) {
                     throw new NumberFormatException();
-                } else if (choice == choices.length - 1) {
+                } else if (back && choice == backChoice) {
                     return "Back";
-                } else if (all && choice == choices.length - 2) {
+                } else if (all && choice == allChoice) {
                     return "All";
                 } else {
-                    return Category.values()[choice - 1].toString();
+                    return categoryByOrder.get(choice).getName();
                 }
             } catch (NumberFormatException e) {
-                view.resultMessage(String.format("Please input a number from 1 to %d", Category.values().length));
+                view.resultMessage(String.format("Please input a number from 1 to %d",
+                        categoryByName.size() + extra));
             }
-
         } while (true);
     }
 }
